3ff94edd63f7e909507287344b1ec09528546385
Lc <641229781@qq.com>
2020-12-29 14:16:12 +0800

    整合swagger

--- service/service_edu/src/main/java/com/hr/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/hr/eduservice/EduApplication.java
@@ -2,8 +2,10 @@ package com.hr.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.hr"})
 public class EduApplication {
 
     public static void main(String[] args) {
--- service/service_edu/src/main/java/com/hr/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/hr/eduservice/controller/EduTeacherController.java
@@ -3,11 +3,11 @@ package com.hr.eduservice.controller;
 
 import com.hr.eduservice.entity.EduTeacher;
 import com.hr.eduservice.service.impl.EduTeacherServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,18 +19,31 @@ import java.util.List;
  * @author testjava
  * @since 2020-12-29
  */
+@Api("讲师管理")
 @RestController
+@CrossOrigin //跨域
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
 
     @Autowired
     private EduTeacherServiceImpl eduTeacherService;
 
+    @ApiOperation("所有讲师列表")
     @GetMapping("/findAll")
     private List<EduTeacher> findAll() {
         List<EduTeacher> list = eduTeacherService.list(null);
         return list;
     }
+
+    @ApiOperation("逻辑删除讲师")
+    @DeleteMapping("/{id}")
+    private boolean removeTeacher(
+                                    @ApiParam(name = "id", value = "讲师ID", required = true)
+                                    @PathVariable("id") String id) {
+        boolean result = eduTeacherService.removeById(id);
+        return result;
+    }
+
 
 }
 
