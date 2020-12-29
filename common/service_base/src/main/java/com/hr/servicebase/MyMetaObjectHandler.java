626bee5eefb28c0b2ee3b5cdcdc617293d9a6438
Lc <641229781@qq.com>
2020-12-29 16:27:51 +0800

    讲师的crud接口完成

---
+++ common/service_base/src/main/java/com/hr/servicebase/MyMetaObjectHandler.java
@@ -0,0 +1,24 @@
+package com.hr.servicebase;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+//创建自动填充类 MyMetaObjectHandler
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+}
--- service/service_edu/src/main/java/com/hr/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/hr/eduservice/controller/EduTeacherController.java
@@ -1,16 +1,22 @@
 package com.hr.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hr.commonutils.R;
 import com.hr.eduservice.entity.EduTeacher;
+import com.hr.eduservice.entity.vo.TeacherQuery;
 import com.hr.eduservice.service.impl.EduTeacherServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -45,6 +51,86 @@ public class EduTeacherController {
         return result ? R.ok() : R.error();
     }
 
+    @ApiOperation("分页查询讲师")
+    @GetMapping("/pageTeacher/{current}/{limit}")
+    private R pageListTeacher(@PathVariable Integer current,
+                              @PathVariable Integer limit) {
+        //分页插件
+        Page<EduTeacher> page = new Page<>(current,limit);
+        //调用方法实现分页
+        //调用方法的时候 底层封装,把分页所有数据封装到page对象里面
+        eduTeacherService.page(page,null);
+        //总记录数
+        long total = page.getTotal();
+        List<EduTeacher> teachers = page.getRecords();
+        Map map = new HashMap();
+        map.put("total", total);
+        map.put("rows", teachers);
+        return R.ok().data(map);
+    }
+
+
+    @ApiOperation("条件分页查询讲师")
+    @PostMapping("/pageListTeacherCondition/{current}/{limit}")
+    private R pageListTeacherCondition(@PathVariable Integer current,
+                                       @PathVariable Integer limit,
+                                       @RequestBody(required = false) TeacherQuery teacherQuery) {
+        //分页插件
+        Page<EduTeacher> page = new Page<>(current,limit);
+        //调用方法实现分页
+        //调用方法的时候 底层封装,把分页所有数据封装到page对象里面
+        QueryWrapper<EduTeacher> queryWrapper = new QueryWrapper<>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        if(!StringUtils.isEmpty(name)) {
+            queryWrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)) {
+            queryWrapper.eq("level", level);
+        }
+        if(!StringUtils.isEmpty(begin)) {
+            // >=
+            queryWrapper.ge("gmt_create", begin);
+        }
+        if(!StringUtils.isEmpty(end)) {
+            // <=
+            queryWrapper.le("gmt_create", end);
+        }
+
+        eduTeacherService.page(page,queryWrapper);
+        //总记录数
+        long total = page.getTotal();
+        List<EduTeacher> teachers = page.getRecords();
+        Map map = new HashMap();
+        map.put("total", total);
+        map.put("rows", teachers);
+        return R.ok().data(map);
+    }
+
+
+    @ApiOperation("添加讲师")
+    @PostMapping("/addTeacher")
+    private R addTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean result = eduTeacherService.save(eduTeacher);
+        return result? R.ok() : R.error();
+    }
+
+
+    @ApiOperation("查询讲师")
+    @GetMapping("/getTeacher/{id}")
+    private R getTeacher(@PathVariable String id) {
+        EduTeacher eduTeacher = eduTeacherService.getById(id);
+        return R.ok().data("teacher", eduTeacher);
+    }
+
+    @ApiOperation("修改讲师")
+    @PostMapping("/updateTeacher")
+    private R updateTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean result = eduTeacherService.updateById(eduTeacher);
+        return result? R.ok() : R.error();
+    }
 
 }
 
