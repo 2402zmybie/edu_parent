6d5378a2f8f1b8568fa1d48baaeb20d2f54f3143
Lc <641229781@qq.com>
2021-01-08 12:59:49 +0800

    vos微服务加入主类

---
+++ service/service_vod/src/main/java/com/hr/vod/VodApplication.java
@@ -0,0 +1,13 @@
+package com.hr.vod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class VodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VodApplication.class, args);
+    }
+}
