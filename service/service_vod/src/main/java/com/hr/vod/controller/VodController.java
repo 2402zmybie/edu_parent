545ac1df40f84c7d0817800e0c8e4bb992ead696
Lc <641229781@qq.com>
2021-01-22 10:38:40 +0800

    整合阿里云视频播放组件

--- service/service_vod/src/main/java/com/hr/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/hr/vod/controller/VodController.java
@@ -3,11 +3,12 @@ package com.hr.vod.controller;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
 import com.hr.commonutils.R;
 import com.hr.servicebase.exceptionhandler.EduException;
 import com.hr.vod.service.VodService;
 import com.hr.vod.utils.ConstantVodProperties;
-import com.hr.vod.utils.InitVodClient;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+
+import static com.hr.vod.utils.InitVodClient.initVodClient;
 
 @RestController
 @RequestMapping("/eduvod/video")
@@ -39,7 +42,7 @@ public class VodController {
     @DeleteMapping("/removeAlyVideo/{id}")
     public R removeAlyVideo(@PathVariable("id") String id) {
         //初始化对象
-        DefaultAcsClient client = InitVodClient.initVodClient(constantVodProperties.getKeyid(), constantVodProperties.getKeysecret());
+        DefaultAcsClient client = initVodClient(constantVodProperties.getKeyid(), constantVodProperties.getKeysecret());
         //创建删除视频的request对象
         DeleteVideoRequest request = new DeleteVideoRequest();
         request.setVideoIds(id);
@@ -60,6 +63,24 @@ public class VodController {
     public R deleteBatch(@RequestParam("videoIdList") List<String> videoIdList) {
         vodService.removeMoreAlyVideo(videoIdList);
         return R.ok();
+    }
+
+
+    @ApiOperation("根据视频id获取视频凭证")
+    @GetMapping("/getPlayAuth/{id}")
+    public R getPlayAuth(@PathVariable String id) {
+        DefaultAcsClient client = initVodClient(constantVodProperties.getKeyid(), constantVodProperties.getKeysecret());
+        GetVideoPlayAuthResponse response = new GetVideoPlayAuthResponse();
+        try {
+            GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+            request.setVideoId(id);
+            response = client.getAcsResponse(request);
+            //播放凭证
+            String playAuth= response.getPlayAuth();
+            return R.ok().data("playAuth", playAuth);
+        } catch (Exception e) {
+            throw new EduException(20001,"获取视频凭证失败");
+        }
     }
 
 }
