c59e2e599c6c60a92129b44cbb52fa832240b3a0
Lc <641229781@qq.com>
2021-01-25 18:04:08 +0800

    s

--- service/service_order/src/main/java/com/hr/eduorder/controller/OrderController.java
+++ service/service_order/src/main/java/com/hr/eduorder/controller/OrderController.java
@@ -1,9 +1,12 @@
 package com.hr.eduorder.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hr.commonutils.JwtUtils;
 import com.hr.commonutils.R;
+import com.hr.eduorder.entity.Order;
 import com.hr.eduorder.service.OrderService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,11 +28,21 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @ApiOperation("新建订单")
     @PostMapping("/createOrder/{courseId}")
     public R saveOrder(@PathVariable String courseId, HttpServletRequest request) {
         //创建订单,返回订单号
         String orderNo = orderService.createOrder(courseId, JwtUtils.getMemberIdByJwtToken(request));
-        return R.ok().data("orderNo", orderNo);
+        return R.ok().data("orderId", orderNo);
+    }
+
+    @ApiOperation("根据订单id查询订单信息")
+    @GetMapping("/getOrderInfo/{orderId}")
+    public R getOrderInfo(@PathVariable("orderId") String orderId) {
+        QueryWrapper<Order> wrapper = new QueryWrapper<>();
+        wrapper.eq("order_no", orderId);
+        Order order = orderService.getOne(wrapper);
+        return R.ok().data("item",order);
     }
 
 }
--- service/service_order/src/main/java/com/hr/eduorder/controller/PayLogController.java
+++ service/service_order/src/main/java/com/hr/eduorder/controller/PayLogController.java
@@ -1,9 +1,13 @@
 package com.hr.eduorder.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hr.commonutils.R;
+import com.hr.eduorder.service.PayLogService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 /**
  * <p>
@@ -14,8 +18,37 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-01-22
  */
 @RestController
-@RequestMapping("/eduorder/pay-log")
+@RequestMapping("/eduorder/paylog")
+@CrossOrigin
 public class PayLogController {
 
+    @Autowired
+    private PayLogService payLogService;
+
+    @ApiOperation("生成微信支付二维码接口")
+    @GetMapping("/createNative/{orderNo}")
+    public R createNative(@PathVariable("orderNo") String orderNo) {
+        //返回信息,包含二维码地址,还有其他信息
+        Map map =  payLogService.createNative(orderNo);
+        return R.ok().data(map);
+    }
+
+
+    @ApiOperation("查询订单支付状态")
+    @GetMapping("/queryPayStatus/{orderNo}")
+    public R queryPayStatus(@PathVariable("orderNo") String orderNo) {
+        //调用查询接口
+        Map<String, String> map = payLogService.queryPayStatus(orderNo);
+        if (map == null) {//出错
+            return R.error().message("支付出错");
+        }
+        if (map.get("trade_state").equals("SUCCESS")) {//如果成功
+            //更改订单状态
+            payLogService.updateOrderStatus(map);
+            return R.ok().message("支付成功");
+        }
+
+        return R.ok().code(25000).message("支付中");
+    }
 }
 
