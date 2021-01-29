f30ae26ddc15530bc90b59f8dd8d91d0f38bf9eb
Lc <641229781@qq.com>
2021-01-29 17:18:23 +0800

    整合spring security,添加默认代码

---
+++ service/service_acl/src/main/java/com/hr/aclservice/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.hr.aclservice.service.impl;
+
+import com.hr.aclservice.entity.UserRole;
+import com.hr.aclservice.mapper.UserRoleMapper;
+import com.hr.aclservice.service.UserRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Service
+public class UserRoleServiceImpl extends ServiceImpl<UserRoleMapper, UserRole> implements UserRoleService {
+
+}
---
+++ service/service_acl/src/main/java/com/hr/aclservice/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.hr.aclservice.service.impl;
+
+import com.hr.aclservice.entity.User;
+import com.hr.aclservice.mapper.UserMapper;
+import com.hr.aclservice.service.UserService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户表 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+    @Override
+    public User selectByUsername(String username) {
+        return baseMapper.selectOne(new QueryWrapper<User>().eq("username", username));
+    }
+}
